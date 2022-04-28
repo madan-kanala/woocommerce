e3c0ff9c29c4c4cdd42b59c3a596cd96ac9c93c8
madan-kanala <mdnkanala.dev@gmail.com>
2022-04-28 22:50:50 +0600

    task 28 update filter title text

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -1,9 +1,9 @@
-import { Container } from "@mui/material";
-import axios from "axios";
-import { useCallback, useEffect, useState } from "react";
-import { useHistory, useParams } from "react-router-dom";
-import Products from "../../components/layout/Products/Products";
-import ProductsBanner from "../../components/layout/Products/ProductsBanner";
+import { Container } from '@mui/material';
+import axios from 'axios';
+import { useCallback, useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import Products from '../../components/layout/Products/Products';
+import ProductsBanner from '../../components/layout/Products/ProductsBanner';
 import {
   Bottom,
   Contenitrice,
@@ -15,21 +15,21 @@ import {
   Sortby,
   Top,
   Wrapper,
-} from "./ProductList.styled";
+} from './ProductList.styled';
 
-const imageBaseUrl = "https://minisobbs.s3.us-east-2.amazonaws.com/Zoho-admin";
+const imageBaseUrl = 'https://minisobbs.s3.us-east-2.amazonaws.com/Zoho-admin';
 
 const images = {
-  "01": imageBaseUrl + "/fragancias+-+banner.png",
-  "02": imageBaseUrl + "/cosmeticos+-+banner.png",
-  "03": imageBaseUrl + "/cuidado+-+banner.png",
-  "04": imageBaseUrl + "/accesorios-banner.png",
-  10: imageBaseUrl + "/vida+-+banner.png",
-  12: imageBaseUrl + "/libreria+-+banner.png",
-  13: imageBaseUrl + "/jueguetes+-+banner.png",
-  14: imageBaseUrl + "/electronicos-banner.png",
-  15: imageBaseUrl + "/textil+-+banner.png",
-  16: imageBaseUrl + "/bolsas+-+banner.png",
+  '01': imageBaseUrl + '/fragancias+-+banner.png',
+  '02': imageBaseUrl + '/cosmeticos+-+banner.png',
+  '03': imageBaseUrl + '/cuidado+-+banner.png',
+  '04': imageBaseUrl + '/accesorios-banner.png',
+  10: imageBaseUrl + '/vida+-+banner.png',
+  12: imageBaseUrl + '/libreria+-+banner.png',
+  13: imageBaseUrl + '/jueguetes+-+banner.png',
+  14: imageBaseUrl + '/electronicos-banner.png',
+  15: imageBaseUrl + '/textil+-+banner.png',
+  16: imageBaseUrl + '/bolsas+-+banner.png',
 };
 const ProductList = () => {
   const { category: cat } = useParams();
@@ -41,9 +41,9 @@ const ProductList = () => {
   const [filters, setFilters] = useState({
     minPrice: 0,
     maxPrice: 0,
-    name: "",
+    name: '',
   });
-  const [sort, setSort] = useState("newest");
+  const [sort, setSort] = useState('newest');
   const history = useHistory();
 
   const handleFilters = async (event) => {
@@ -52,7 +52,7 @@ const ProductList = () => {
       history.push(`/productoslista/${value}`);
       // history.go(`/productoslista/${value}`);
     } catch (err) {
-      console.log("- - - - - err: ", err);
+      console.log('- - - - - err: ', err);
     }
     /*
       setFilters({
@@ -65,7 +65,7 @@ const ProductList = () => {
   const [categories, setCategories] = useState([]);
 
   const getCategories = useCallback(async () => {
-    const catUrl = "http://3.16.73.177:9080/public/categories/first";
+    const catUrl = 'http://3.16.73.177:9080/public/categories/first';
     const res = await axios.get(catUrl, {
       crossDomain: true,
     });
@@ -76,7 +76,7 @@ const ProductList = () => {
   useEffect(() => {
     getCategories();
   }, [getCategories]);
-  const [image, setImage] = useState("");
+  const [image, setImage] = useState('');
 
   useEffect(() => setImage(images[cat]), [cat]);
 
@@ -89,16 +89,18 @@ const ProductList = () => {
             <br />
             <br />
             <p></p>
-            <Top className="row gx-0 gx-sm-3">
+            <Top className='row gx-0 gx-sm-3'>
               {/*<TopButton>CONTINUE SHOPPING</TopButton>*/}
               {/* <TopTexts><TopText>No. Resultados</TopText></TopTexts> */}
 
-              <FilterContainerResponsive className="col-12 col-sm-6 col-md-3 ">
-                <h6>Categories</h6>
+              {/* I have another comment, Could you add titles to the filters? for the first it would be "Categorías:" for the Second "" for the Third "Precio Máximo:" for the search "Buscador de Productos:" and for the fifth "Ordena por precio:" */}
+
+              <FilterContainerResponsive className='col-12 col-sm-6 col-md-3 '>
+                <h6>Categorías</h6>
                 <Select1
                   value={cat}
-                  name="categoría"
-                  className="form-control"
+                  name='categoría'
+                  className='form-control'
                   onChange={handleFilters}
                 >
                   {categories.map((category) => (
@@ -108,14 +110,14 @@ const ProductList = () => {
                   ))}
                 </Select1>
               </FilterContainerResponsive>
-              <div className="row col-12 col-sm-6 col-md-3 gx-0 gx-sm-2 mb-2 mb-sm-0">
-                <div className="col-6">
-                  <h6>Min Price</h6>
+              <div className='row col-12 col-sm-6 col-md-3 gx-0 gx-sm-2 mb-2 mb-sm-0'>
+                <div className='col-6'>
+                  <h6>Precio Mínimo:</h6>
                   <PriceInput
-                    type="number"
-                    name="minPrice"
-                    placeholder="min"
-                    className="form-control"
+                    type='number'
+                    name='minPrice'
+                    placeholder='min'
+                    className='form-control'
                     value={filters.minPrice}
                     onChange={(e) => {
                       const value = e.target.value;
@@ -128,13 +130,13 @@ const ProductList = () => {
                     }}
                   />
                 </div>
-                <div className="col-6">
-                  <h6>Max Price</h6>
+                <div className='col-6'>
+                  <h6>Precio Máximo:</h6>
                   <PriceInput
-                    type="number"
-                    name="maxPrice"
-                    placeholder="max"
-                    className="form-control "
+                    type='number'
+                    name='maxPrice'
+                    placeholder='max'
+                    className='form-control '
                     value={filters.maxPrice}
                     onChange={(e) => {
                       const value = e.target.value;
@@ -148,12 +150,12 @@ const ProductList = () => {
                   />
                 </div>
               </div>
-              <div className="col-12 col-sm-6 mb-1 mb-sm-0 col-md-3 ">
-                <h6>Search...</h6>
+              <div className='col-12 col-sm-6 mb-1 mb-sm-0 col-md-3 '>
+                <h6>Buscador de Productos:</h6>
                 <PriceInput
-                  type="text"
-                  placeholder="search..."
-                  className="form-control"
+                  type='text'
+                  placeholder='search...'
+                  className='form-control'
                   value={filters.name}
                   onChange={(e) => {
                     setFilters((prev) => ({
@@ -163,17 +165,17 @@ const ProductList = () => {
                   }}
                 />
               </div>
-              <Sortby className="col-12 col-sm-6 col-md-3 ">
+              <Sortby className='col-12 col-sm-6 col-md-3 '>
                 <h6>Sort By</h6>
                 <Select1
                   onChange={(e) => setSort(e.target.value)}
-                  className="form-control"
+                  className='form-control'
                 >
-                  <Option value="newest">Ordenar </Option>
-                  <Option Option value="desc">
+                  <Option value='newest'>Ordenar </Option>
+                  <Option Option value='desc'>
                     Precio más alto
                   </Option>
-                  <Option value="asc">Precio más bajo</Option>
+                  <Option value='asc'>Precio más bajo</Option>
                 </Select1>
               </Sortby>
             </Top>
