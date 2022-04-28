525eae3ed82a2be00fb01a2f2d7312fce66d94a3
madan-kanala <mdnkanala.dev@gmail.com>
2022-04-28 16:09:12 +0600

    related to task 29. problem: blank page show after login status: solved

--- src/components/layout/Profile/OrderHistory.js
+++ src/components/layout/Profile/OrderHistory.js
@@ -1,13 +1,12 @@
-import axios from "axios";
-import moment from "moment";
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import axios from 'axios';
+import moment from 'moment';
+import React, { useEffect, useState } from 'react';
 const OrderHistory = () => {
   const [orders, setOrders] = useState([]);
-  const user_name = useSelector((state) => state.user.currentUser?.user_name);
 
   useEffect(() => {
-    const url = `http://3.16.73.177:9080/public/products/allorderwithcustomer?user_name=${user_name}`;
+    const username = localStorage.getItem('username');
+    const url = `http://3.16.73.177:9080/public/products/allorderwithcustomer?user_name=${username}`;
     axios
       .get(url)
       .then((res) => {
@@ -15,14 +14,14 @@ const OrderHistory = () => {
         console.log(res.data?.body);
       })
       .catch((e) => console.log(e));
-  }, [user_name]);
+  }, []);
 
   return (
     <div>
-      <table class="table">
+      <table class='table'>
         <tbody>
           <tr>
-            <th scope="row">No. De Order</th>
+            <th scope='row'>No. De Order</th>
             <td>Estodo</td>
             <td>Fecha registrado</td>
             <td>No. De Productions</td>
@@ -33,7 +32,7 @@ const OrderHistory = () => {
             <tr key={order.id}>
               <td>#{order.id}</td>
               <td>{order.state}</td>
-              <td>{moment(order.fecha).format("L")}</td>
+              <td>{moment(order.fecha).format('L')}</td>
               <td></td>
               <td></td>
               <td>Q{order.granTotal}</td>
