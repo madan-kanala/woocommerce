76a141c753719876c47c48e82bef651ceea609c0
madan-kanala <mdnkanala.dev@gmail.com>
2022-04-29 16:56:56 +0600

    task 43 is done

--- src/components/layout/Carousel/MultiItemCarousel.jsx
+++ src/components/layout/Carousel/MultiItemCarousel.jsx
@@ -1,21 +1,22 @@
 //import { multiData } from "../../../data";
-import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
-import { Container } from "@mui/material";
-import axios from "axios";
-import React, { useCallback, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick-theme.css";
-import "slick-carousel/slick/slick.css";
-import "./MultiItemCarousel.css";
-import classes from "./MultiItemCarousel.module.css";
+import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
+import { Container } from '@mui/material';
+import axios from 'axios';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import './MultiItemCarousel.css';
+import classes from './MultiItemCarousel.module.css';
 
 const PreviousBtn = (props) => {
   console.log(props);
   const { className, onClick } = props;
   return (
     <div className={className} onClick={onClick}>
-      <ArrowBackIos style={{ color: "blue", fontSize: "25px" }} />
+      <ArrowBackIos style={{ color: 'blue', fontSize: '25px' }} />
     </div>
   );
 };
@@ -24,24 +25,31 @@ const NextBtn = (props) => {
   const { className, onClick } = props;
   return (
     <div className={className} onClick={onClick}>
-      <ArrowForwardIos style={{ color: "blue", fontSize: "25px" }} />
+      <ArrowForwardIos style={{ color: 'blue', fontSize: '25px' }} />
     </div>
   );
 };
 
 const MultiItemCarousel = () => {
   // const [state, setState] = React.useState(false);
+  const { isAuthenticated } = useSelector((state) => state.user);
 
   const [slides, setSlides] = useState([]);
 
   const getSlides = useCallback(async () => {
-    let url =
-      "http://3.16.73.177:9080/public/products/carrusel?carrusel=CARRUSEL1";
+    const userName = localStorage.getItem('username');
+    const getUrl = () => {
+      if (isAuthenticated) {
+        return `http://3.16.73.177:9080/public/products/carrusel?carrusel=CARRUSEL1&userName=${userName}`;
+      }
+      return 'http://3.16.73.177:9080/public/products/carrusel?carrusel=CARRUSEL1';
+    };
+    let url = getUrl();
     const res = await axios.get(url, {
       crossDomain: true,
     });
     setSlides(res.data.body);
-  }, []);
+  }, [isAuthenticated]);
   useEffect(() => {
     getSlides();
   }, [getSlides]);
@@ -81,8 +89,9 @@ const MultiItemCarousel = () => {
   return (
     <Container>
       <div className='Wrapper'>
-        <div style={{ margin: "30px" }}>
-          <h1>Ofertas</h1>
+        <div style={{ margin: '30px' }}>
+          <h1>Productos Recomendados</h1>
+
           <Slider {...settings}>
             {slides.map((slide) => (
               <Card slide={slide} />
@@ -98,22 +107,22 @@ const Card = ({ slide }) => {
   const imagen = slide.url;
 
   return (
-    <div style={{ textAlign: "center" }}>
+    <div style={{ textAlign: 'center' }}>
       <Link
         to={`/product/${slide.productosPkDto.codInt}/${slide.productosPkDto.barra}`}
         style={{
-          textDecoration: "none",
+          textDecoration: 'none',
         }}
       >
         <img
           className='multi_image'
-          src={imagen + "-1.jpg"}
+          src={imagen + '-1.jpg'}
           alt=''
           style={{
-            width: "100%",
-            height: "190px",
-            objectFit: "contain",
-            marginBottom: "10px",
+            width: '100%',
+            height: '190px',
+            objectFit: 'contain',
+            marginBottom: '10px',
           }}
         />
         <p className={classes.Description}>{slide.descripcion}</p>
