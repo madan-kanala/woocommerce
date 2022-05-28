0a1598af805e0eab6ff848b4c44ae14d22e2fc2f
madan-kanala <mdnkanala.dev@gmail.com>
2022-05-28 18:34:17 +0600

    task 25 is done.

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,15 +1,15 @@
-import { Container } from "@mui/material";
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { toast } from "react-toastify";
+import { Container } from '@mui/material';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import {
   removeProductFromCart,
   updateCartFromServer,
-} from "../../redux/cartAction";
-import { categories } from "./../../data";
-import "./Cart.css";
-import classes from "./Cart.module.css";
+} from '../../redux/cartAction';
+import { categories } from './../../data';
+import './Cart.css';
+import classes from './Cart.module.css';
 import {
   BarraId,
   Bottom,
@@ -34,21 +34,21 @@ import {
   Title,
   Top,
   Wrapper,
-} from "./Cart.styled";
+} from './Cart.styled';
 const Cart = () => {
   const { total } = useSelector((state) => state.cart);
   const { isAuthenticated } = useSelector((state) => state.user);
   const cartProductsList = useSelector((state) => state.cart.products);
   const dispatch = useDispatch();
-  let username = localStorage.getItem("username");
+  let username = localStorage.getItem('username');
   if (!username) {
   }
 
   useEffect(() => {
-    if (isAuthenticated) {
+    if (isAuthenticated && cartProductsList.length === 0) {
       dispatch(updateCartFromServer());
     }
-  }, [dispatch, isAuthenticated]);
+  }, [dispatch, isAuthenticated, cartProductsList]);
 
   const handleClick = (codInt, barra) => {
     dispatch(removeProductFromCart(codInt, barra));
@@ -78,7 +78,7 @@ const Cart = () => {
                 cartProductsList.map((product) => (
                   <Product id={product.id} key={Math.random()}>
                     <ProductDetail>
-                      <Image src={product.url + "-1.jpg"} />
+                      <Image src={product.url + '-1.jpg'} />
                       <Details>
                         <ProductName>
                           <b>Producto:</b>
@@ -96,7 +96,7 @@ const Cart = () => {
                     </ProductDetail>
                     <PriceDetail>
                       <p
-                        className="btn-floating btn-fb mx-1"
+                        className='btn-floating btn-fb mx-1'
                         onClick={(e) => {
                           e.preventDefault();
                           handleClick(
@@ -106,7 +106,7 @@ const Cart = () => {
                           );
                         }}
                       >
-                        <i className="fa-solid fa-trash TrashIcon"></i>
+                        <i className='fa-solid fa-trash TrashIcon'></i>
                       </p>
                       <ProductAmountContainer>
                         {/*<Add />*/}
@@ -128,25 +128,25 @@ const Cart = () => {
                 <SummaryItemText>Subtotal</SummaryItemText>
                 <SummaryItemPrice>Q. {total}</SummaryItemPrice>
               </SummaryItem>
-              <SummaryItem type="total">
+              <SummaryItem type='total'>
                 <SummaryItemText>Total</SummaryItemText>
                 <SummaryItemPrice>Q. {total}</SummaryItemPrice>
               </SummaryItem>
 
               {isAuthenticated ? (
                 cartProductsList.length > 0 ? (
-                  <Link to="/payment" className={classes.link2}>
+                  <Link to='/payment' className={classes.link2}>
                     <button className={classes.bTnProperty}>
                       Comprar Ahora
                     </button>
                   </Link>
                 ) : (
                   <>
-                    <Link to="#" className={classes.link2}>
+                    <Link to='#' className={classes.link2}>
                       <button
                         className={classes.bTnProperty}
                         onClick={() => {
-                          toast.error("Cart is empty");
+                          toast.error('Cart is empty');
                         }}
                       >
                         Comprar Ahora
